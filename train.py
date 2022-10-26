4467dd28b49c81970fec95b1e70d64b336cdbcd5
arefeen <krsultan069@gmail.com>
2022-10-26 11:01:28 -0600

    code updated.

--- train.py
+++ train.py
@@ -4,14 +4,32 @@ from typing import Dict
 
 import torch
 from coral_pytorch.dataset import corn_label_from_logits
-from coral_pytorch.losses import corn_loss
+from coral_pytorch.losses import corn_loss, coral_loss
 from matplotlib import pyplot as plt
 from torch import nn
 from torch.utils.data import DataLoader
 
 
+def compute_loss_and_metrics(model: nn.Module, dataloader: DataLoader, device, num_classes=5):
+    model.eval()
+    with torch.no_grad():
+        mae, mse, running_loss = 0.0, 0.0, 0.0
+        for i, data in enumerate(dataloader):
+            features, targets = data['input'], data['target']
+            features = features.to(device)
+            targets = targets.to(device)
+            targets = targets.flatten()
+            logits = model(features)
+            loss = corn_loss(logits, targets, num_classes)
+            # statistics
+            mse += torch.sum((corn_label_from_logits(logits).float() - targets) ** 2)
+            mae += torch.sum(torch.abs(corn_label_from_logits(logits).float() - targets))
+            running_loss += loss.item() * features.size(0)
+        return running_loss, mae, mse
+
+
 def train_model(model: nn.Module, optimizer, scheduler, dataloaders: Dict[str, DataLoader],
-                device, dataset_sizes: Dict[str, int], num_epochs=25, num_classes=5):
+                device, num_epochs=25, num_classes=5):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -30,10 +48,6 @@ def train_model(model: nn.Module, optimizer, scheduler, dataloaders: Dict[str, D
             else:
                 model.eval()  # Set model to evaluate mode
 
-            running_loss = 0.0
-            running_corrects = 0
-            mae, mse = 0., 0.
-
             # Iterate over data.
             for data in dataloaders[phase]:
                 inputs, labels = data['input'], data['target']
@@ -47,7 +61,7 @@ def train_model(model: nn.Module, optimizer, scheduler, dataloaders: Dict[str, D
                 # track history if only in train
                 with torch.set_grad_enabled(phase == 'train'):
                     outputs = model(inputs)
-                    preds = corn_label_from_logits(outputs).float()
+                    # preds = corn_label_from_logits(outputs).float()
 
                     loss = corn_loss(outputs, labels, num_classes)
 
@@ -56,18 +70,16 @@ def train_model(model: nn.Module, optimizer, scheduler, dataloaders: Dict[str, D
                         loss.backward()
                         optimizer.step()
 
-                # statistics
-                mae += torch.sum(torch.abs(preds - labels))
-                mse += torch.sum((preds - labels) ** 2)
-                running_loss += loss.item() * inputs.size(0)
                 # running_corrects += torch.sum(preds == labels.data)
             if phase == 'train':
                 scheduler.step()
 
-            epoch_loss = running_loss / dataset_sizes[phase]
+            running_loss, mae, mse = compute_loss_and_metrics(model, dataloaders[phase], device, num_classes)
+
+            epoch_loss = running_loss / len(dataloaders[phase])
             # epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            epoch_mae = mae.item() / dataset_sizes[phase]
-            epoch_mse = mse.item() / dataset_sizes[phase]
+            epoch_mae = mae.item() / len(dataloaders[phase])
+            epoch_mse = mse.item() / len(dataloaders[phase])
             all_epochs_mse[phase].append(epoch_mse)
 
             print(f'{phase} Loss: {epoch_loss:.4f}, MAE: {epoch_mae},  MSE: {epoch_mse:.4f}')
@@ -87,6 +99,7 @@ def train_model(model: nn.Module, optimizer, scheduler, dataloaders: Dict[str, D
     plt.plot(all_epochs_mse["train"], label="train")
     plt.plot(all_epochs_mse["test"], label="test")
     plt.legend()
+    plt.savefig("mse_loss.png")
     plt.show()
 
     # save model weights
