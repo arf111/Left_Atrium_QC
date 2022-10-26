b34cb3ecbd3861468a08fa7bda1648b53dc10ff8
arefeen <krsultan069@gmail.com>
2022-10-26 11:01:51 -0600

    Removed Normalization.

--- train_qc_model.py
+++ train_qc_model.py
@@ -1,7 +1,8 @@
 import torch
 from monai.data import DataLoader
 from torch import optim
-from torchvision.transforms import Compose, RandomResizedCrop, RandomHorizontalFlip, ToTensor, Normalize, Resize
+from torchvision.transforms import Compose, RandomResizedCrop, RandomHorizontalFlip, ToTensor, Normalize, Resize, \
+    PILToTensor
 
 from data.atria_dataset import AtriaDataset
 import segmentation_models_pytorch as smp
@@ -11,19 +12,26 @@ from train import train_model
 
 NUM_CLASSES = 5
 RESIZE_IMG = 256
+NUM_EPOCHS = 25
+
+
+def get_dataloader(root_dir):
+    train_transform = Compose([Resize(RESIZE_IMG), RandomHorizontalFlip(), ToTensor()])
+    val_transform = Compose([Resize(RESIZE_IMG), ToTensor()])
+
+    train_dataset = AtriaDataset(root_dir, split_name="training_set2", transform=train_transform)
+    val_dataset = AtriaDataset(root_dir, split_name="testing_set2", transform=val_transform)
+
+    train_loader = DataLoader(dataset=train_dataset, num_workers=8, batch_size=32, shuffle=True)
+    val_loader = DataLoader(dataset=val_dataset, num_workers=8, batch_size=32, shuffle=True)
+
+    return train_loader, val_loader
+
 
 if __name__ == "__main__":
     root_dir = 'dataset'
 
-    train_transform = Compose([RandomResizedCrop(RESIZE_IMG), RandomHorizontalFlip(),
-                               ToTensor(), Normalize((0.5,), (0.5,))])
-    test_transform = Compose([Resize(RESIZE_IMG), ToTensor(), Normalize((0.5,), (0.5,))])
-
-    train_dataset = AtriaDataset(root_dir, split_name="training_set", transform=train_transform)
-    train_loader = DataLoader(dataset=train_dataset, num_workers=8, batch_size=16, shuffle=True)
-
-    test_dataset = AtriaDataset(root_dir, split_name="testing_set", transform=test_transform)
-    test_loader = DataLoader(dataset=test_dataset, num_workers=8, batch_size=16, shuffle=True)
+    train_loader, val_loader = get_dataloader(root_dir)
 
     # device
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -43,8 +51,8 @@ if __name__ == "__main__":
     # scheduler
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
 
-    dataloaders = {"train": train_loader, "test": test_loader}
-    dataset_sizes = {"train": len(train_dataset), "test": len(test_dataset)}
+    dataloaders = {"train": train_loader, "test": val_loader}
+    dataset_sizes = {"train": len(train_loader.dataset), "test": len(val_loader.dataset)}
 
-    best_model = train_model(model=qc_model, dataloaders=dataloaders, dataset_sizes=dataset_sizes, optimizer=optimizer,
-                             scheduler=scheduler, device=device, num_epochs=2, num_classes=NUM_CLASSES)
+    best_model = train_model(model=qc_model, dataloaders=dataloaders, optimizer=optimizer,
+                             scheduler=scheduler, device=device, num_epochs=NUM_EPOCHS, num_classes=NUM_CLASSES)
